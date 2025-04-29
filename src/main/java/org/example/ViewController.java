b4f76f1a10aef1bc99eae3c4b160ffc7d592b46a
Shwetha Sakthidharan <shwethasakthidharan@gmail.com>
2025-04-29 10:55:51 +0530

    Initial commit

--- src/main/java/org/example/ViewController.java
+++ src/main/java/org/example/ViewController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
+
     @GetMapping("/")
     public String index() {
-        return "forward:/index.html";  // Will look in all static locations
+        return "forward:/index.html";
     }
 }
